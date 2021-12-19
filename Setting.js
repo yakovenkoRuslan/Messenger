ab43943a21408a63bf76b736e79cf618706f98dc
PLATnya <vadim.plataniy@gmail.com>
2021-12-19 16:41:38 +0200

    Add sign out button in settings

--- Setting.js
+++ Setting.js
@@ -2,13 +2,14 @@ import * as React from 'react';
 import {Button, View, Text } from 'react-native';
 
 
-export function SettingsScreen({ navigation }) {
+export function SettingsScreen(props) {
+    const { signOut } = React.useContext(props.auth);
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Details Screen</Text>
         <Button
-          title="Go to Home"
-          onPress={() => navigation.push('Home')}
+          title="SignOut"
+          onPress={() => signOut()}
         />
       </View>
     );
